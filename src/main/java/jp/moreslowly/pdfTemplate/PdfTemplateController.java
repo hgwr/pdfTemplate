8e8ba2d65e9023be2bd4acf395c3d22ac7103f45
Shigeru Hagiwara <hgwrsgr@gmail.com>
2020-06-20 13:27:32 +0900

    initial commit

---
+++ src/main/java/jp/moreslowly/pdfTemplate/PdfTemplateController.java
@@ -0,0 +1,11 @@
+package jp.moreslowly.pdfTemplate;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class PdfTemplateController {
+  @RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+}
---
+++ src/test/java/jp/moreslowly/pdfTemplate/PdfTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.moreslowly.pdfTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
