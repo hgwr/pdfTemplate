a104471a951c732e67e68677ae486ac38553add9
Shigeru Hagiwara <hgwrsgr@gmail.com>
2020-06-20 16:29:13 +0900

    application/json header

--- src/main/java/jp/moreslowly/pdfTemplate/PdfTemplateController.java
+++ src/main/java/jp/moreslowly/pdfTemplate/PdfTemplateController.java
@@ -1,4 +1,5 @@
 package jp.moreslowly.pdfTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,7 @@ public class PdfTemplateController {
 		return "Greetings from Spring Boot!";
 	}
 
-	@RequestMapping("/hello")
+	@RequestMapping(path = "/hello", produces = MediaType. APPLICATION_JSON_VALUE)
 	public String hello() {
 		return "'Hello'";
 	}
