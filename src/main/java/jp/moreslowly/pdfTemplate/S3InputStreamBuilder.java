8accacbbc36be6b2ffeff459773ea0fa2961e86d
Shigeru Hagiwara <hgwrsgr@gmail.com>
2020-06-20 18:32:23 +0900

    s3 feature

--- src/main/java/jp/moreslowly/pdfTemplate/S3InputStreamBuilder.java
+++ src/main/java/jp/moreslowly/pdfTemplate/S3InputStreamBuilder.java
@@ -1,46 +1,22 @@
 package jp.moreslowly.pdfTemplate;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "template")
+@ConfigurationProperties(prefix = "app.template")
 public class S3InputStreamBuilder {
-    private String bucketName;
+    private String bucket;
 
-    public static InputStream build(String keyName) {
+    public InputStream build(String keyName) throws IOException {
         final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
-        try {
-            S3Object o = s3.getObject(bucketName, keyName);
-            S3ObjectInputStream s3is = o.getObjectContent();
-            FileOutputStream fos = new FileOutputStream(new File(keyName));
-            byte[] read_buf = new byte[1024];
-            int read_len = 0;
-            while ((read_len = s3is.read(read_buf)) > 0) {
-                fos.write(read_buf, 0, read_len);
-            }
-            s3is.close();
-            fos.close();
-        } catch (AmazonServiceException e) {
-            System.err.println(e.getErrorMessage());
-            System.exit(1);
-        } catch (FileNotFoundException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        }
+        S3Object o = s3.getObject(bucket, keyName);
+        return o.getObjectContent();
     }
 }
